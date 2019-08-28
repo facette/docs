65f0885e570ac83121afe03648ca1ca9e5023879
Vincent Batoufflet <vincent@batoufflet.info>
2019-08-28 06:24:32 +0200

    Update Gulp setup to latest versions

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,11 @@ var fs = require('fs'),
     chmod = require('gulp-chmod'),
     concat = require('gulp-concat'),
     environments = require('gulp-environments'),
-    footer = require('gulp-footer'),
     header = require('gulp-header'),
+    merge = require('merge-stream'),
     myth = require('gulp-myth'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    uglifycss = require('gulp-uglifycss'),
-    vendor = require('gulp-concat-vendor');
+    terser = require('gulp-terser'),
+    uglifycss = require('gulp-uglifycss');
 
 var config = {
     pkg: JSON.parse(fs.readFileSync('./bower.json')),
@@ -59,46 +57,48 @@ var config = {
     }
 };
 
-gulp.task('default', [
-    'build'
-]);
-
-gulp.task('build', [
-    'build-style',
-    'copy-script',
-    'copy-style',
-]);
-
-gulp.task('copy-script', function() {
-    gulp.src(config.files.vendor.js)
-        .pipe(vendor('vendor.js'))
-        .pipe(environments.production(uglify()))
-        .pipe(chmod(644))
+function copyScript() {
+    return gulp.src(config.files.vendor.js)
+        .pipe(concat('vendor.js'))
+        .pipe(environments.production(terser()))
+        .pipe(chmod(0o644))
         .pipe(gulp.dest('static/assets/js'));
-});
+}
 
-gulp.task('build-style', function() {
-    gulp.src(config.files.style)
+function buildStyle() {
+    return gulp.src(config.files.style)
         .pipe(concat('style.css'))
         .pipe(header(config.banner + '\n', {pkg: config.pkg}))
         .pipe(myth())
         .pipe(environments.production(uglifycss()))
-        .pipe(chmod(644))
+        .pipe(chmod(0o644))
         .pipe(gulp.dest('static/assets/css'));
-});
+}
 
-gulp.task('copy-style', function() {
-    gulp.src(config.files.vendor.css)
-        .pipe(vendor('vendor.css'))
-        .pipe(environments.production(uglifycss()))
-        .pipe(chmod(644))
-        .pipe(gulp.dest('static/assets/css'));
+function copyStyle() {
+    return merge(
+        gulp.src(config.files.vendor.css)
+            .pipe(concat('vendor.css'))
+            .pipe(environments.production(uglifycss()))
+            .pipe(chmod(0o644))
+            .pipe(gulp.dest('static/assets/css')),
 
-    gulp.src(config.files.vendor.fonts)
-        .pipe(chmod(644))
-        .pipe(gulp.dest('static/assets/fonts'));
+        gulp.src(config.files.vendor.fonts)
+            .pipe(chmod(0o644))
+            .pipe(gulp.dest('static/assets/fonts')),
 
-    gulp.src(config.files.vendor.images)
-        .pipe(chmod(644))
-        .pipe(gulp.dest('static/assets/images'));
-});
+        gulp.src(config.files.vendor.images)
+            .pipe(chmod(0o644))
+            .pipe(gulp.dest('static/assets/images'))
+    );
+}
+
+var build = gulp.series(
+    buildStyle,
+    copyScript,
+    copyStyle,
+);
+
+exports.build = build;
+
+exports.default = build;
